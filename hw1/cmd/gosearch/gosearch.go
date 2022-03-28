a8dd3f26f945988d8b135a454b4c726bb2d81b96
HalamBalam <antonsurganov@yandex.ru>
2022-03-28 11:55:22 +0300

    hw1 corrections #1

--- hw1/cmd/gosearch/gosearch.go
+++ hw1/cmd/gosearch/gosearch.go
@@ -14,12 +14,25 @@ var s *string
 func main() {
 	flag.Parse()
 
-	docs := make([]crawler.Document, 30)
-	scan(&docs, "https://go.dev")
-	scan(&docs, "https://golang.org/")
+	docs := []crawler.Document{}
+	sites := [...]string{"https://go.dev", "https://golang.org/"}
+	for i := 0; i < len(sites); i++ {
+		siteDocs, err := scan(sites[i])
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		for _, doc := range siteDocs {
+			docs = append(docs, doc)
+		}
+	}
 
 	if *s != "" {
-		find_refs(&docs)
+		refs := findRefs(docs)
+		for _, ref := range refs {
+			fmt.Println(ref)
+		}
+		fmt.Printf("Найдено %d ссылок", len(refs))
 	}
 }
 
@@ -27,29 +40,19 @@ func init() {
 	s = flag.String("s", "", "search text")
 }
 
-func scan(docs *[]crawler.Document, site string) {
+func scan(site string) ([]crawler.Document, error) {
 	log.Println("Сканирование сайта " + site)
 
 	scanner := spider.New()
-	new_docs, err := scanner.Scan(site, 2)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	for _, doc := range new_docs {
-		*docs = append(*docs, doc)
-	}
+	return scanner.Scan(site, 2)
 }
 
-func find_refs(docs *[]crawler.Document) {
-	i := 0
-	for _, doc := range *docs {
+func findRefs(docs []crawler.Document) ([]string) {
+	res := []string{}
+	for _, doc := range docs {
 		if strings.Contains(doc.Title, *s) || strings.Contains(doc.Body, *s) {
-			fmt.Println(doc.URL)
-			i++
+			res = append(res, doc.URL)
 		}
 	}
-
-	fmt.Printf("Найдено %d ссылок", i)
+	return res
 }
